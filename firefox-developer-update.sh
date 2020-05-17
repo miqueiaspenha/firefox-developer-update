0097910de334599afa08f014568f8f9fde281c35
Miqueias Penha <miqueiaspenha@gmail.com>
2020-05-17 15:10:20 -0300

    melhorando as informações de log

--- firefox-developer-update.sh
+++ firefox-developer-update.sh
@@ -1,11 +1,12 @@
-!/bin/bash sh
+!/bin/sh
 
-# Deletando quando versão antiga
+echo "Deletando quando versão antiga..."
+
 sudo rm -Rf /opt/firefox-developer*
 sudo rm -Rf /usr/bin/firefox-developer
 sudo rm -Rf /usr/share/applications/firefox-developer.desktop
 
-# Baixando e instalando a nova versão
+echo "Baixando e instalando a nova versão..."
 wget "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=pt-BR" -O firefox-developer.tar.bz2
 sudo tar -jxvf  firefox-developer.tar.bz2 -C /opt/
 sudo mv /opt/firefox*/ /opt/firefox-developer
@@ -13,12 +14,14 @@ sudo ln -sf /opt/firefox-developer/firefox /usr/bin/firefox-developer
 echo -e '[Desktop Entry]\n Version=77.0\n Encoding=UTF-8\n Name=Mozilla Firefox\n Comment=Navegador Web\n Exec=/opt/firefox-developer/firefox\n Icon=/opt/firefox-developer/browser/chrome/icons/default/default128.png\n Type=Application\n Categories=Network' | sudo tee /usr/share/applications/firefox-developer.desktop
 sudo chmod +x /usr/share/applications/firefox-developer.desktop
 
-#Baixando e instalando o updater
+echo "Baixando e instalando o updater..."
 wget https://raw.githubusercontent.com/miqueiaspenha/firefox-developer-update/master/firefox-developer-update.sh
 sudo mv firefox-developer-update.sh /opt/firefox-developer/firefox-developer-update.sh
 sudo chmod +x /opt/firefox-developer/firefox-developer-update.sh
 sudo ln -sf /opt/firefox-developer/firefox-developer-update.sh /usr/bin/firefox-developer-update
 sudo chmod +x /opt/firefox-developer/firefox-developer-update.sh
 
-# Deletando o download
+echo "Deletando os arquivos baixados..."
 sudo rm -Rf firefox-developer.tar.bz2
+
+echo "Firefox Developer instalado!"
